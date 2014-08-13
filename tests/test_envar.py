c6702ff522acd8570a7b43ee278833578889f24f
Harshad Sharma <harshad@sharma.io>
2014-08-13 15:57:40 +0530

    Moar tests!

--- tests/test_envar.py
+++ tests/test_envar.py
@@ -13,7 +13,20 @@ class TestEnvar(unittest.TestCase):
         If variable does not exist and no default is given,
         envar should raise an exception.
         """
-        self.assertRaises(ImproperlyConfigured, envar, "TOWEL", str)
+        self.assertRaises(ImproperlyConfigured, envar, 'TOWEL', str)
+
+    def test_env_var_available_with_no_default(self):
+        os.environ['LEWOT'] = '42'
+        self.assertEqual(envar('LEWOT', int), 42)
+
+    def test_env_var_available_with_default(self):
+        os.environ['LEWOT'] = '42'
+        self.assertEqual(envar('LEWOT', int, '24'), 42)
+
+    def test_env_var_available_with_default_verbose(self):
+        os.environ['LEWOT'] = '42'
+        self.assertEqual(envar('LEWOT', int, '24', verbose=True), 42)
+        #todo: check for warning
 
     # Integers
     def test_missing_int_with_default_int(self):
@@ -25,12 +38,21 @@ class TestEnvar(unittest.TestCase):
     def test_missing_int_with_default_str(self):
         self.assertRaises(ImproperlyConfigured, envar, 'TOWEL', int, 'Forty Two')
 
+    def test_missing_int_with_default_bool(self):
+        self.assertRaises(ImproperlyConfigured, envar, 'TOWEL', int, 'True')
+
     # Booleans
     def test_missing_bool_with_default_bool(self):
         self.assertEqual(envar('TOWEL', bool, 'True'), True)
 
     def test_missing_bool_with_default_str(self):
         self.assertRaises(ImproperlyConfigured, envar, 'TOWEL', bool, 'Forty Two')
+
+    def test_missing_bool_with_default_int(self):
+        self.assertRaises(ImproperlyConfigured, envar, 'TOWEL', bool, '42')
+
+    def test_missing_bool_with_default_float(self):
+        self.assertRaises(ImproperlyConfigured, envar, 'TOWEL', bool, '42.42')
 
     # Strings
     def test_missing_str_with_default_str(self):
